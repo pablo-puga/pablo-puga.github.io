b6c8154d789f874c55a2bbcf95f06a2186f6e605
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-09 18:08:04 +0200

    Added symmary and socials links

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,49 @@
 <script>
-    let name = 'world';
+    import Socials from './Socials.svelte';
+    import { faTwitter, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+    const rrss = [
+        {
+            name: 'Twitter',
+            icon: faTwitter.icon,
+            display: '@Malevolo92',
+            link: 'https://twitter.com/Malevolo92',
+            color: '#00acee',
+        },
+        {
+            name: 'Linkedin',
+            icon: faLinkedin.icon,
+            display: 'Pablo Puga Peralta',
+            link: 'https://www.linkedin.com/in/pablopugaperalta',
+            color: '#0e76a8',
+        },
+        {
+            name: 'GitHub',
+            icon: faGithub.icon,
+            display: 'pablo-puga',
+            link: 'https://github.com/pablo-puga',
+            color: '#6e5494',
+        },
+    ];
 </script>
 
-<h1 class="text-xl text-red-600">Hello {name}! How are you?</h1>
+<article class="font-sans text-gray-800 flex flex-col items-center gap-5">
+    <h1 class="text-4xl font-bold text-center mt-16 mb-5 tracking-wider">Pablo Puga Peralta</h1>
+
+    <section class="text-center p-3 md:p-2 lg:p-0 md:w-3/4 transition-all duration-100">
+        <h2 class="text-2xl md:text-3xl font-medium mb-3">Who am I?</h2>
+        <p class="text-base md:text-xl">
+            I am a Telecom Engineer specialized on modern communication technologies. I like to develop things in JavaScript, PHP, C and C++ and I am a great fan of automatize all possible tasks. 
+        </p>
+    </section>
+
+    <!-- Trabajo actual -->
+
+    <!-- Aptitudes -->
+    <!-- https://www.npmjs.com/package/d3-cloud -->
+
+    <Socials {rrss}/>
+</article>
 
 <style global lang="postcss">
     @tailwind base;
---
+++ src/Socials.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let rrss;
+</script>
+
+<section class="flex flex-col items-center">
+    <h2 class="text-2xl md:text-3xl font-medium mb-3">You can find me at</h2>
+    <ul class="flex flex-row flex-wrap justify-center gap-5 text-xl">
+        {#each rrss as network}
+        <article class="social flex flex-row items-center transition-all duration-200 hover:text-2xl" style={`--hover-color: ${network.color}`}>
+            <svg
+                aria-hidden="true"
+                class="fa-icon"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox={`0 0 ${network.icon[0]} ${network.icon[1]}`}
+            >
+                <path fill="currentColor" d={network.icon[4]}/>
+            </svg>
+            <h3 class="inline-block ml-1">
+                <a href={network.link} title={network.name}>{network.display}</a>
+            </h3>
+        </article>
+        {/each}
+    </ul>
+</section>
+
+<style>
+    .fa-icon {
+        overflow: hidden;
+        width: 1em;
+        height: 1em;
+        display: inline-block;
+    }
+
+    .social:hover {
+        color: var(--hover-color);
+    }
+</style>
