17f31eef65d56652505ca8f35b896e1c65597e01
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-11 12:22:05 +0200

    Updated build paths and changed the favicon png for an ico

--- rollup.config.js
+++ rollup.config.js
@@ -32,10 +32,10 @@ function serve() {
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'public/app.js'
 	},
 	plugins: [
 		svelte({
@@ -59,7 +59,7 @@ export default {
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
+		css({ output: 'app.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
