740e95a50822af2a817dffcdd4a96ca144aae454
Pablo Puga <pablo.puga.git@gmail.com>
2021-10-14 12:03:00 +0200

    Extracted the fa icons to their own component

--- src/Socials.svelte
+++ src/Socials.svelte
@@ -1,5 +1,6 @@
 <script>
     import Section from './Section.svelte';
+    import FaIcon from './FaIcon.svelte';
 
     export let rrss = [];
 </script>
@@ -8,15 +9,7 @@
     <ul class="flex flex-row flex-wrap justify-center gap-5 text-xl">
         {#each rrss as network}
         <li class="social flex flex-row items-center transition-all duration-200 hover:text-2xl" style={`--hover-color: ${network.color}`}>
-            <svg
-                aria-hidden="true"
-                class="fa-icon"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox={`0 0 ${network.icon[0]} ${network.icon[1]}`}
-            >
-                <path fill="currentColor" d={network.icon[4]}/>
-            </svg>
+            <FaIcon path={network.icon[4]} width={network.icon[0]} height={network.icon[1]}/>
             <h3 class="inline-block ml-1">
                 <a href={network.link} title={network.name}>{network.display}</a>
             </h3>
@@ -26,13 +19,6 @@
 </Section>
 
 <style>
-    .fa-icon {
-        overflow: hidden;
-        width: 1em;
-        height: 1em;
-        display: inline-block;
-    }
-
     .social:hover {
         color: var(--hover-color);
     }
