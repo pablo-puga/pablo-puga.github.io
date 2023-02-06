f3e7840e57a793196e3b487db53b0fea0ed90281
Pablo Puga <pablo.puga.git@gmail.com>
2023-02-06 12:30:01 +0100

    Adjusted the hover efect of the socials

--- src/Socials.svelte
+++ src/Socials.svelte
@@ -8,7 +8,7 @@
 <Section title="You can find me at">
     <ul class="flex flex-row flex-wrap justify-center gap-5 text-xl">
         {#each rrss as network}
-        <li class="social flex flex-row items-center transition-all duration-200 hover:text-2xl" style={`--hover-color: ${network.color}`}>
+        <li class="social flex flex-row items-center transition-all duration-200 hover:underline" style={`--hover-color: ${network.color}`}>
             <FaIcon path={network.icon[4]} width={network.icon[0]} height={network.icon[1]}/>
             <h3 class="inline-block ml-1">
                 <a href={network.link} title={network.name}>{network.display}</a>
